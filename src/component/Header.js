97e5576a20c732807a0d7a25d511e80900db78d1
priyanshu1430 <priyanshugpt143@gmail.com>
2022-11-19 18:58:18 +0530

    push

---
+++ src/component/Header.js
@@ -0,0 +1,14 @@
+import '../App.css';
+
+function Header(props) {
+  return (
+    <div className='flex shopping-cart'>
+        <div onClick={() => props.handleShow(false)}>Shopping Cart</div>
+        <div onClick={() => props.handleShow(true)}>Cart
+            <sup> {props.count} </sup>
+        </div>
+    </div>
+  );  
+}
+
+export default Header;
---
+++ src/component/ProductList.js
@@ -0,0 +1,26 @@
+import '../App.css';
+function ProductList({product,addToCart}){
+    return(
+        <div className="flex">
+        {
+            product.map((productItem, productIndex)=>{
+                return (
+                    <div style={{width: '50%'}}>
+                        <div className='product-item'>
+                            <img src={productItem.url} width="50%"/>
+                            <p>{productItem.name}  {productItem.category}</p>
+                            <p>{productItem.seller}</p>
+                            <p>Rs.{productItem.price}/-</p>
+                            <button onClick={() => addToCart(productItem)}>Add to cart</button>
+                        </div>
+
+                        </div>
+                )
+            })
+
+        }
+        </div>
+
+    )
+}
+export default ProductList
